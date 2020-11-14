378baa229511f3f859fc784b204fd8d436e40958
sksat <sksat@sksat.net>
2020-11-14 22:24:46 +0900

    update: LSP, fix: signify, add: lexima

---
+++ files/.config/nvim/lua/nvim-lsp.lua
@@ -0,0 +1,7 @@
+require'nvim_lsp'.clangd.setup{}
+require'nvim_lsp'.cmake.setup{}
+require'nvim_lsp'.dockerls.setup{}
+require'nvim_lsp'.gopls.setup{}
+require'nvim_lsp'.rust_analyzer.setup{}
+require'nvim_lsp'.texlab.setup{}
+require'nvim_lsp'.yamlls.setup{}
