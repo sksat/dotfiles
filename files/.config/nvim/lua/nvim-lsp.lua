d1dfcc77875b6f9e3b1cd1002299e896f25fc39d
sksat <sksat@sksat.net>
2020-12-11 06:10:20 +0900

    update: LSP, add: load default screen config in xinitrc

--- files/.config/nvim/lua/nvim-lsp.lua
+++ files/.config/nvim/lua/nvim-lsp.lua
@@ -1,7 +1,13 @@
+local lsp_status = require('lsp-status')
+lsp_status.register_progress()
+
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.cmake.setup{}
 require'lspconfig'.dockerls.setup{}
 require'lspconfig'.gopls.setup{}
-require'lspconfig'.rust_analyzer.setup{}
+require'lspconfig'.rust_analyzer.setup{
+	on_attach = lsp_status.on_attach,
+	capabilities = lsp_status.capabilities
+}
 require'lspconfig'.texlab.setup{}
 require'lspconfig'.yamlls.setup{}
