226f3f4d0c3f2566e6d02c1a1604d070690fca0d
sksat <sksat@sksat.net>
2021-09-16 10:02:52 +0900

    ugokan

--- config/nvim/lua/nvim-lsp.lua
+++ config/nvim/lua/nvim-lsp.lua
@@ -4,24 +4,24 @@ lsp_status.register_progress()
 
 lspcfg.clangd.setup{}
 lspcfg.cmake.setup{}
-lspcfg.dockerls.setup{}
+--lspcfg.dockerls.setup{}
 lspcfg.elmls.setup{}
 lspcfg.gopls.setup{}
 lspcfg.html.setup{
 	cmd = {"vscode-html-languageserver", "--stdio"}
 }
-lspcfg.julials.setup{}
+--lspcfg.julials.setup{}
 --lspcfg.pyls.setup{}
 lspcfg.rome.setup{}
 lspcfg.rust_analyzer.setup{
 --	on_attach = lsp_status.on_attach,
 	capabilities = lsp_status.capabilities
 }
-lspcfg.sumneko_lua.setup{}
+--lspcfg.sumneko_lua.setup{}
 lspcfg.texlab.setup{}
 lspcfg.vimls.setup{}
 lspcfg.yamlls.setup{}
-lspcfg.zls.setup{}
+--lspcfg.zls.setup{}
 
 local saga = require 'lspsaga'
 saga.init_lsp_saga()
