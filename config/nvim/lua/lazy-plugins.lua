47fcbca624c42c1d4828fc117744c0a52b0487f2
sksat <sksat@sksat.net>
2023-05-06 18:40:37 +0900

    lazy.nvim: add completion with LSP by cmp-vsnip, nvim-cmp

--- config/nvim/lua/lazy-plugins.lua
+++ config/nvim/lua/lazy-plugins.lua
@@ -98,4 +98,47 @@ return {
       require('fidget').setup({})
     end,
   },
+
+  -- completion with LSP
+  {
+    'hrsh7th/vim-vsnip',
+    dependencies = {
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+    },
+    init = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-j>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' }, -- For vsnip users.
+          -- { name = 'luasnip' }, -- For luasnip users.
+          -- { name = 'ultisnips' }, -- For ultisnips users.
+          -- { name = 'snippy' }, -- For snippy users.
+        }, {
+          { name = 'buffer' },
+        })
+      })
+    end,
+  },
 }
